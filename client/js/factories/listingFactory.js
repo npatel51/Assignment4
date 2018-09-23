4a2da3d0f06b6b0f31370dd87dba5e8c5ecdcc45
Niraj <npatel3@ufl.edu>
2018-09-23 19:30:32 -0400

    removed localhost since this is being run on web server

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,18 +1,18 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('//localhost:8080/api/listings');
+      return $http.get('/api/listings');
     },
 	
 	  create: function(listing) {
-	  return $http.post('//localhost:8080/api/listings', listing);
+	  return $http.post('/api/listings', listing);
     }, 
 
     delete: function(listing) {
 	   /**TODO
         return result of HTTP delete method
        */
-      return $http.delete('//localhost:8080/api/listings/'+listing._id,listing);
+      return $http.delete('/api/listings/'+listing._id,listing);
     }
   };
 
