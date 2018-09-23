cf354cd4ff0bb7d17f194d6bada878230dc240fd
Niraj <npatel3@ufl.edu>
2018-09-22 23:31:16 -0400

    updated PORT

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,5 @@ module.exports = {
     db: {
       uri: 'mongodb://np1:nirajptl1@ds119442.mlab.com:19442/listings', //place the URI of your mongo database here.
     },
-    port: 8080
+    port: 8080 
   };
